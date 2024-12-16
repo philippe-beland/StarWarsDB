4239c6c2f25764b382b2d4376164ce868204c7ca
philippebeland <phil_beland@hotmail.com>
2024-12-15 20:50:01 -0500

    Cleaned Data

--- process_uuid.py
+++ process_uuid.py
@@ -2,8 +2,7 @@ import json
 from pathlib import Path
 import uuid
 
-current_dir: Path = Path(__file__).parent
-data_dir: Path = Path(current_dir, "Raw Data")
+data_dir: Path = Path(__file__).parent
 
 
 class process_UUID:
@@ -21,72 +20,86 @@ class process_UUID:
         # Loop through file in folder
         for file in data_dir.iterdir():
             if file.stem == "Sources":
-                self.sources = json.load(open(file, "r"))
+                pass
+                # self.sources = json.load(open(file, "r"))
             elif "Source" in file.stem:
-                self.sources_files[file.stem.lower()] = json.load(open(file, "r"))
+                pass
+                # self.sources_files[file.stem] = json.load(open(file, "r"))
             else:  # Entities
-                self.entities_files[file.stem.lower()] = json.load(open(file, "r"))
+                self.entities_files[file.stem] = json.load(open(file, "r"))
+
+        cleaned_path = Path(data_dir, "Cleaned Data")
+        for file in cleaned_path.iterdir():
+            if "Source" in file.stem:
+                self.sources_files[file.stem] = json.load(open(file, "r"))
 
     def generate_uuid(self):
         """
         Generate a UUID for each source and entity.
-
-        Returns:
-            dict: A dictionary of sources and entities with UUID.
         """
-        self.converting_id_to_uuid = {}
         for type, entity in self.entities_files.items():
+            self.converting_id_to_uuid = {}
             print(type)
 
-    #         new_id = str(uuid.uuid4())
-    #         self.converting_id_to_uuid[source["source_id"]] = new_id
-    #         source["source_id"] = new_id
+            for type_source, source in self.sources_files.items():
+                if type_source == f"Source{type}":
+                    for e in entity:
+                        new_id = str(uuid.uuid4())
+                        self.converting_id_to_uuid[e["id"]] = new_id
+                        e["id"] = new_id
 
-    #     new_source_file = Path(data_dir, "Sources_uuid.json")
-    #     with open(new_source_file, "w") as f:
-    #         json.dump(sources, f, indent=4)
+                        for s in source:
+                            if s[type[:-1]] in self.converting_id_to_uuid.keys():
+                                s[type[:-1]] = self.converting_id_to_uuid[s[type[:-1]]]
 
-    # def process_personnages(self):
-    #     personnages = (
-    #         self.arkham_perso + self.dcau_perso + self.films_perso + self.gotham_perso
-    #     )
-    #     for perso in personnages:
-    #         perso["source_id"] = self.converting_id_to_uuid[perso["source_id"]]
+                        new_source_file = Path(
+                            data_dir, "Processed Data", f"{type_source}_uuid.json"
+                        )
+                        with open(new_source_file, "w") as f:
+                            json.dump(source, f, indent=4)
 
-    #     new_perso_file = Path(data_dir, "Personnages_uuid.json")
-    #     with open(new_perso_file, "w") as f:
-    #         json.dump(personnages, f, indent=4)
+            new_entity_file = Path(data_dir, "Processed Data", f"{type}_uuid.json")
+            with open(new_entity_file, "w") as f:
+                json.dump(entity, f, indent=4)
 
 
-# x = process_UUID()
-# x.load_files(data_dir)
-# x.generate_uuid()
+x = process_UUID()
+x.load_files(Path(data_dir, "Raw Data"))
+x.generate_uuid()
 
-for file in data_dir.iterdir():
-    if file.stem == "Sources":
-        pass
-    elif "Source" in file.stem:
-        new_list = []
-        x = json.load(open(file, "r"))
-        for y in x:
-            # if "Source" in y.keys():
-            if len(y.keys()) > 1:
-                if "ID_Test" in y.keys():
-                    y.pop("ID_Test")
 
-                if "ID_test" in y.keys():
-                    y.pop("ID_test")
+def clean_json(data_dir: Path):
+    """
+    Clean the JSON files in the data directory.
 
-                if "ID - Test" in y.keys():
-                    y.pop("ID - Test")
+    Args:
+        data_dir (Path): The path to the data directory.
+    """
+    for file in data_dir.iterdir():
+        if file.stem == "Sources":
+            pass
+        elif "Source" in file.stem:
+            new_list = []
+            x = json.load(open(file, "r"))
+            for y in x:
+                # if "Source" in y.keys():
+                if len(y.keys()) > 1:
+                    if "ID_Test" in y.keys():
+                        y.pop("ID_Test")
 
-                if "Alignement" in y.keys():
-                    y.pop("Alignement")
+                    if "ID_test" in y.keys():
+                        y.pop("ID_test")
 
-                if len(y.keys()) < 3:
-                    print(file, y)
+                    if "ID - Test" in y.keys():
+                        y.pop("ID - Test")
 
-                new_list.append(y)
+                    if "Alignement" in y.keys():
+                        y.pop("Alignement")
 
-        with open(f"Cleaned Data/{file.stem}_cleaned.json", "w") as f:
-            json.dump(new_list, f, indent=4)
+                    if len(y.keys()) < 3:
+                        print(file, y)
+
+                    new_list.append(y)
+
+            with open(f"Cleaned Data/{file.stem}_cleaned.json", "w") as f:
+                json.dump(new_list, f, indent=4)
