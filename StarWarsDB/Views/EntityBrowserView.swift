f4e6a42ab1c34c3143fc2ea22f7210ddec67d487
philippebeland <phil_beland@hotmail.com>
2025-07-19 19:57:14 -0400

    Update EntityBrowserView.swift

--- StarWarsDB/Views/EntityBrowserView.swift
+++ StarWarsDB/Views/EntityBrowserView.swift
@@ -1,115 +1,91 @@
 import SwiftUI
 
+struct EntityBrowserImageView: View {
+    let imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 50, height: 50)
+            .clipShape(Circle())
+    }
+}
+
 struct EntityBrowserView: View {
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    // Replace the ForEach with explicit NavigationLinks
+                    // Characters
                     NavigationLink(destination: EntitiesListView<Character>()) {
                         HStack {
-                            Image(Character.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Character.exampleImageName)
                             Text(Character.displayName)
                         }
                     }
                     
+                    // Species
                     NavigationLink(destination: EntitiesListView<Species>()) {
                         HStack {
-                            Image(Species.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Species.exampleImageName)                           
                             Text(Species.displayName)
                         }
                     }
                     
+                    // Planets
                     NavigationLink(destination: EntitiesListView<Planet>()) {
                         HStack {
-                            Image(Planet.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Planet.exampleImageName)
                             Text(Planet.displayName)
                         }
                     }
                     
+                    // Organizations
                     NavigationLink(destination: EntitiesListView<Organization>()) {
                         HStack {
-                            Image(Organization.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Organization.exampleImageName)
                             Text(Organization.displayName)
                         }
                     }
                     
+                    // Starships
                     NavigationLink(destination: EntitiesListView<Starship>()) {
                         HStack {
-                            Image(Starship.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Starship.exampleImageName)
                             Text(Starship.displayName)
                         }
                     }
                     
+                    // Starship Models
                     NavigationLink(destination: EntitiesListView<StarshipModel>()) {
                         HStack {
-                            Image(StarshipModel.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: StarshipModel.exampleImageName)
                             Text(StarshipModel.displayName)
                         }
                     }
                     
+                    
+                    // Creatures
                     NavigationLink(destination: EntitiesListView<Creature>()) {
                         HStack {
-                            Image(Creature.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Creature.exampleImageName)
                             Text(Creature.displayName)
                         }
                     }
 
+                    // Droids
                     NavigationLink(destination: EntitiesListView<Droid>()) {
                         HStack {
-                            Image(Droid.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Droid.exampleImageName)
                             Text(Droid.displayName)
                         }
                     }
 
+                    // Varias
                     NavigationLink(destination: EntitiesListView<Varia>()) {
                         HStack {
-                            Image(Varia.exampleImageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
+                            EntityBrowserImageView(imageName: Varia.exampleImageName)
                             Text(Varia.displayName)
                         }
                     }
